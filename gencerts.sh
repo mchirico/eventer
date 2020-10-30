9d55f8594fec8ec06ab6e11d7c778523922618ed
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-10-30 16:31:00 -0400

    update-automated-save

--- gencerts.sh
+++ gencerts.sh
@@ -2,7 +2,7 @@
 rm -rf tls-gen
 git clone https://github.com/michaelklishin/tls-gen tls-gen
 cd tls-gen/basic
-make regen CN=webhook-server.webhook-demo.svc
+make regen CN=eventer-server.eventer-demo.svc
 cd result
 
 openssl pkcs12 -in client_key.p12 -out clientCom.pem -nodes \
